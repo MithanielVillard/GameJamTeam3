66fbac5caea611438927e376152b38441ca24f73
LoganDANgtech <147987555+LoganDANgtech@users.noreply.github.com>
2025-11-29 14:26:09 +0100

    [ADD] playermovement

---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class PlayerMovement : CharacterBody2D
+{
+	public const float Speed = 60.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 direction = Input.GetVector("Left","Right","Up","Down");
+
+		if (direction != Vector2.Zero)
+		{
+			this.Velocity = direction * Speed;
+		}
+		else
+		{
+			this.Velocity = Vector2.Zero;
+		}
+		
+		//GetParent<Player>().test();
+		
+		MoveAndSlide();
+	}
+}
